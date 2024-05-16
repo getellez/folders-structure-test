29169dd47b0b99e309866dadda8966d69c2aa8d9
German Tellez <gertellezv@gmail.com>
2024-05-16 13:15:55 -0500

    Add unit tests for groupFolders libs

--- src/__tests__/libs/utils.test.ts
+++ src/__tests__/libs/utils.test.ts
@@ -2,6 +2,47 @@ import { groupFolders, validateOperation } from "../../libs/utils";
 
 describe("utils", () => {
   describe("groupFolders", () => {
+    it("should create subfolders when there are existing parent folders", () => {
+      const folders = {
+        foods: { name: "foods", subfolders: [] },
+        "foods/fruits": { name: "fruits", subfolders: [] },
+      };
+
+      const groupedFolders = groupFolders(folders);
+
+      const parentFolder = groupedFolders.find(
+        (folder) => folder.name === "foods"
+      );
+
+      expect(parentFolder?.subfolders?.length).toBe(1);
+      expect(parentFolder?.subfolders?.[0].name).toBe("fruits");
+    });
+    it("should transform the folder structure in an array", () => {
+      const folders = {
+        movies: { name: "movies", subFolders: [] },
+        foods: { name: "foods", subFolders: [] },
+        "foods/fruits": { name: "fruits", subFolders: [] },
+        "foods/fruits/oranges": { name: "oranges", subFolders: [] },
+      };
+
+      const groupedFolders = groupFolders(folders);
+
+      // Additional test to cover adding a folder to the subfolders array
+      const parentFolder = groupedFolders.find(
+        (folder) => folder.name === "foods"
+      );
+      if (parentFolder && parentFolder.subfolders) {
+        const initialSubfoldersLength = parentFolder.subfolders.length;
+        const newFolder = { name: "newFolder", subFolders: [] };
+        parentFolder.subfolders.push(newFolder);
+        const updatedSubfoldersLength = parentFolder.subfolders.length;
+
+        expect(updatedSubfoldersLength).toBeGreaterThan(
+          initialSubfoldersLength
+        );
+        expect(parentFolder.subfolders).toContain(newFolder);
+      }
+    });
     it("should transform the folder structure in an array", () => {
       const folders = {
         movies: { name: "movies" },
